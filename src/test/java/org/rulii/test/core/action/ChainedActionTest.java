1870112eb68be7a6a43890d41571821b934e9124
Algorithmx <max.arul@algorithmx.com>
2025-01-12 19:01:58 -0500

    Cleaning up JUnit tests

--- src/test/java/org/rulii/test/core/action/ChainedActionTest.java
+++ src/test/java/org/rulii/test/core/action/ChainedActionTest.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.rulii.test.action;
+package org.rulii.test.core.action;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
--- src/test/java/org/rulii/test/core/condition/ConditionTest.java
+++ src/test/java/org/rulii/test/core/condition/ConditionTest.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.rulii.test.condition;
+package org.rulii.test.core.condition;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
