b634088b2adc85e03aade109d1161873d88eca3a
Algorithmx <max.arul@algorithmx.com>
2026-03-22 15:22:24 -0400

    working scripting features

--- src/test/java/org/rulii/test/script/js/ScriptProcessorBugTest.java
+++ src/test/java/org/rulii/test/script/js/ScriptProcessorBugTest.java
@@ -1,9 +1,28 @@
+/*
+ * This software is licensed under the Apache 2 license, quoted below.
+ *
+ * Copyright (c) 1999-2026, Algorithmx Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.rulii.test.script.js;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.rulii.context.RuleContext;
-import org.rulii.script.*;
+import org.rulii.script.DefaultScriptProcessorRegistry;
+import org.rulii.script.Script;
+import org.rulii.script.ScriptProcessorRegistry;
 import org.rulii.script.jsr223.JSR223ScriptProcessor;
 
 import javax.script.*;
